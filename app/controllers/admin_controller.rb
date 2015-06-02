312132a2cf9724635c0f41a8f2275c0bd7417205
Amandeep Singh <amandees@thoughtworks.com>
2015-06-02 18:24:03 +0530

    changing files from git history..app is now not working properly so leaving it now..will do it someday later

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,13 +2,36 @@ class AdminController < ApplicationController
 
   def admin_login
 
-    @result = Admin.where(email_id: params[:email],password: params[:password])
+    @result = Admin.where(username: params[:username],password: params[:password])
     render :admin_home unless @result.empty?
+  end
+
+  def add_section
+
+    @type = params[:type]
+  end
+
+  def admin_login1
 
   end
+
+
+
+  def create_section
+    Section.create(section_name: params[:section_name],description: params[:description], location: params[:location], incharge: params[:incharge])
+    render :admin_home
+  end
+
+  def view_section
+
+    @type = params[:type]
+  end
+
+
 
   def post_params
     params.require(:admin).permit(:email,:password)
   end
 
 end
+
